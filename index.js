2516402380fe6c60604eadf4ec0a643133a99384
Karen Kanana <kananakaren21@gmail.com>
2022-08-04 21:29:49 +0300

    complete practice

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Follow along with the examples here
+/*function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+
+  function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  }
+  sayHelloToIsabel();
+
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  }
+  sayHelloToSofia();
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  }
+  sayHelloToBrendan();*/
+  function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo("Isabel"); // "Hello, Isabel!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1); // "Hello, 1!"
+
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Julio", "hello");
+
